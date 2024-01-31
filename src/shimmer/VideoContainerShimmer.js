2c10ca28865c928af7d2dba11b3d91696a4df7c1
Ritik Sachan <sachanritik1@gmail.com>
2024-01-31 16:14:25 +0530

    Refactor VideoContainerShimmer component

--- src/shimmer/VideoContainerShimmer.js
+++ src/shimmer/VideoContainerShimmer.js
@@ -4,10 +4,13 @@ const VideoContainerShimmer = () => {
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 p-2">
       {Array.from({ length: 12 }).map((value, index) => (
-        <div
-          key={index}
-          className="col-span-1 row-span-1 w-80 h-72 bg-gray-100 "
-        ></div>
+        <div key={index} className="col-span-1 row-span-1 bg-gray-100 ">
+          <img
+            className="rounded-lg"
+            alt="thumbnail"
+            src={"shimmerVideo.png"}
+          />
+        </div>
       ))}
     </div>
   );
