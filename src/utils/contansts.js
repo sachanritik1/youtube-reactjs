adbb6aa4e4666654127b2edd45e5a27c40167770
Ritik Sachan <sachanritik1@gmail.com>
2024-02-11 22:20:16 +0530

    Update Google search API URL

--- src/utils/contansts.js
+++ src/utils/contansts.js
@@ -3,7 +3,7 @@ const GOOGLE_API_KEY = "AIzaSyC4h3a0XYma2wiHz6u615hE-8dp89FzBys";
 const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${GOOGLE_API_KEY}`;
 
 const GOOGLE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 const YOUTUBE_SEARCH_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}&q=`;
 
